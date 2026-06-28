b1683c56b01e54bc8714db768fa859cc0a503de3
l-ferrante <lorenzo.ferrante04@gmail.com>
2026-06-28 20:43:43 +0200

    Update script_a_milano.js

--- materiale/conversione/script_a_milano.js
+++ materiale/conversione/script_a_milano.js
@@ -26,9 +26,6 @@ $(document).ready(function(){
             indice_sez_attuale ++;
 
             gestione_cambio_pagina(indice_sez_attuale);
-            console.log(indice_sez_attuale);
-            console.log(lista_sezioni.length-1);
-
             gestione_firma(indice_sez_attuale);
         }
     })
@@ -45,8 +42,8 @@ $(document).ready(function(){
             $(sezione_precedente).show();
             indice_sez_attuale --;
             
-            gestione_cambio_pagina(indice_sez_attuale)
-
+            gestione_cambio_pagina(indice_sez_attuale);
+            gestione_firma(indice_sez_attuale);
         }
     })
 
@@ -76,13 +73,72 @@ $(document).ready(function(){
         }
     }
 
+    // La firma viene mostrata soltanto sotto l'ultima sezione
     function gestione_firma(indice_sez_attuale){
-    if (indice_sez_attuale == (lista_sezioni.length - 1)){
-        $(".firma").show();
-    } else {
-        $(".firma").hide(); 
+        if (indice_sez_attuale == (lista_sezioni.length - 1)){
+            $(".firma").show();
+        } else {
+            $(".firma").hide(); 
+        }
     }
-}
 
+
+    $("#button_persone").on("click", function(){
+        $("#lista_persone").slideToggle(400);
+        $("#button_persone img").toggleClass("ruotata");
+    })
+
+    $("#button_luoghi").on("click", function(){
+        $("#lista_luoghi").slideToggle(400);
+        $("#button_luoghi img").toggleClass("ruotata");
+    })
+
+    $("#button_organizzazioni").on("click", function(){
+        $("#lista_organizzazioni").slideToggle(400);
+        $("#button_organizzazioni img").toggleClass("ruotata");
+    })
+
+
+    // Gestione dei button per la visualizzazione degli elemementi codificati
+    $("button#tutti").on("click", function(){
+        if ($(".notevole").not(".grassetto").length > 0) {
+            $(".notevole, .term, .pers, .place, .org").addClass("grassetto");
+        } else {
+            $(".notevole, .term, .pers, .place, .org").removeClass("grassetto");
+        }
+    });
+
+    $("button#term").on("click", function(){
+        $(".term").toggleClass("grassetto");
+    })
+
+    $("button#persone").on("click", function(){
+        $(".pers").toggleClass("grassetto");
+    })
+
+    $("button#luoghi").on("click", function(){
+        $(".place").toggleClass("grassetto");
+    })
+
+    $("button#organizzazioni").on("click", function(){
+        $(".org").toggleClass("grassetto");
+    })
+
+
+    $(".notevole").on("click", function(){
+        var posizioneGlossario = $("#glossario").offset().top;
+        $("html, body").animate({
+            scrollTop: posizioneGlossario
+        });
+    });
+
+    $(window).on("scroll", gestoreFreccia);
+    function gestoreFreccia(){
+        if ($(window).scrollTop() > 1500) {
+            $("#torna_su").css("opacity", "1");
+        } else {
+            $("#torna_su").css("opacity", "0");
+        }
+    }
 
 })
